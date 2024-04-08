30f2333857d0f8b9772c1f62eb4f40bc91cbb22d
kang <gjsdms1244@gmail.com>
2024-04-08 09:33:36 +0900

    Feat : Upload initial setup file

---
+++ src/main/java/com/kosta/humanstory/HumanstoryApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.humanstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanstoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanstoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kosta/humanstory/HumanstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.humanstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanstoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
