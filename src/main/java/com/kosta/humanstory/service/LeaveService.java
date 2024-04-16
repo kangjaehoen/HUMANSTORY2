d84dc514093428f8e3e4c6cdb41ddec81aea1fb8
ROEN <hide924@naver.com>
2024-04-16 11:06:13 +0900

    Main

---
+++ src/main/java/com/kosta/humanstory/service/LeaveService.java
@@ -0,0 +1,11 @@
+package com.kosta.humanstory.service;
+
+
+import com.kosta.humanstory.domain.LeaveRequestVO;
+
+
+import java.util.List;
+
+public interface LeaveService {
+    public List<LeaveRequestVO> getList();
+}
